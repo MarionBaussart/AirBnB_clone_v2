80edbef553220e2a886cf0d7ec8e488ad0d863c7
MarionBaussart <marion.baussart@gmail.com>
2022-04-28 08:15:18 +0000

    update T2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,6 @@
 """ Fabric script (based on the file 1-pack_web_static.py) that distributes
 an archive to your web servers, using the function do_deploy """
 
-import time
 from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['35.237.22.35', '35.237.118.104']
@@ -13,14 +12,15 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
+        name_file = archive_path.split("/")[-1]
+        new_path = ("/data/web_static/releases/" + name_file.spilt(".")[0])
+
         """ Upload the archive to the /tmp/ directory of the web server """
         put(archive_path, "/tmp/")
 
         """ Uncompress the archive to the folder
         /data/web_static/releases/<archive filename without extension>
         on the web server """
-        name_file = archive_path.split("/")[-1]
-        new_path = ("/data/web_static/releases/" + name_file.spilt(".")[0])
         run("mkdir -p {}/".format(new_path))
         run("tar -xzf /tmp/{} -C {}/".format(name_file, new_path))
 
