cfb3f5d58c9346a84c99fb3e3abf5e6ae3744bae
MarionBaussart <marion.baussart@gmail.com>
2022-04-28 11:20:03 +0000

    T2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,28 +15,28 @@ def do_deploy(archive_path):
         name_file = archive_path.split("/")[-1]
         new_path = ("/data/web_static/releases/" + name_file.spilt(".")[0])
 
-        """ Upload the archive to the /tmp/ directory of the web server """
-        put(archive_path, "/tmp/")
+        """Upload the archive to the /tmp/ directory of the web server"""
+        put(archive_path, '/tmp/')
 
-        """ Uncompress the archive to the folder
+        """Uncompress the archive to the folder
         /data/web_static/releases/<archive filename without extension>
-        on the web server """
-        run("mkdir -p {}/".format(new_path))
-        run("tar -xzf /tmp/{} -C {}/".format(name_file, new_path))
+        on the web server"""
+        run('mkdir -p {}/'.format(new_path))
+        run('tar -xzf /tmp/{} -C {}/'.format(name_file, new_path))
 
-        """ Delete the archive from the web server """
-        run("rm /tmp/{}".format(name_file))
-        run("mv {}/web_static/* {}/".format(new_path, new_path))
-        run("rm -rf {}/web_static".format(new_path))
+        """Delete the archive from the web server"""
+        run('rm /tmp/{}'.format(name_file))
+        run('mv {}/web_static/* {}/'.format(new_path, new_path))
+        run('rm -rf {}/web_static'.format(new_path))
 
-        """ Delete the symbolic link /data/web_static/current
-        from the web server """
-        run("rm -rf /data/web_static/current")
+        """Delete the symbolic link /data/web_static/current
+        from the web server"""
+        run('rm -rf /data/web_static/current')
 
-        """ Create a new the symbolic link /data/web_static/current
+        """Create a new the symbolic link /data/web_static/current
         on the web server, linked to the new version of your code
-        (/data/web_static/releases/<archive filename without extension>) """
-        run("ln -s {}/ /data/web_static/current".format(new_path))
+        (/data/web_static/releases/<archive filename without extension>)"""
+        run('ln -s {}/ /data/web_static/current'.format(new_path))
         return True
     except Exception:
         return False
